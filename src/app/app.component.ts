a860fa6efd5a063c8aa5cada85ae16c36ec21c71
vishal2468 <vishalpoddar2468@gmail.com>
2023-09-14 07:03:13 +0530

    select control validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,12 @@ import { User } from './user';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+isTopicValid=false;
+validateTopic(topic: any) {
+  if(topic==='default') this.isTopicValid=false;
+  else this.isTopicValid=true
+}
   title = 'tdf';
   topics = ["react","angular","vue"];
-  userModel=new User('','rob@test.com',1234567890,'','Morning',true);
+  userModel=new User('','rob@test.com',1234567890,'default','Morning',true);
 }
