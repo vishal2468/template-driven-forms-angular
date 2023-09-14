abe27f95b4c337a45d27cba7f1f1e6e64d37ed33
vishal2468 <vishalpoddar2468@gmail.com>
2023-09-14 07:34:42 +0530

    submitting form data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +8,23 @@ import { User } from './user';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-isTopicValid=false;
-validateTopic(topic: any) {
-  if(topic==='default') this.isTopicValid=false;
-  else this.isTopicValid=true
-}
+
+  isTopicValid = false;
   title = 'tdf';
-  topics = ["react","angular","vue"];
-  userModel=new User('','rob@test.com',1234567890,'default','Morning',true);
+  topics = ["react", "angular", "vue"];
+  userModel = new User('', 'rob@test.com', 1234567890, 'default', 'Morning', true);
+
+  constructor(private _enrollmentService: EnrollmentService) {
+
+  }
+
+  onSubmit() {
+    this._enrollmentService.enroll(this.userModel).subscribe((data: any) => console.log("success", data),
+      (faliure: any) => console.log("faliure", faliure));
+  }
+
+  validateTopic(topic: any) {
+    if (topic === 'default') this.isTopicValid = false;
+    else this.isTopicValid = true
+  }
 }
